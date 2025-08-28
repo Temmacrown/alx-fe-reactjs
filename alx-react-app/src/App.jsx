2e9ad3a954991bd58237b1446faf3696ff34143b
Temmacrown <temmacrown41@gmail.com>
2025-08-28 02:33:16 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -13,22 +14,14 @@ function App() {
 }
 
 export default App;
-import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <UserProfile 
         name="Alice" 
-        age="25" 
+        age={25} 
         bio="Loves hiking and photography" 
-      />
-      
-      {/* You can reuse the component with different props */}
-      <UserProfile 
-        name="John" 
-        age="30" 
-        bio="A software developer who enjoys traveling." 
       />
     </div>
   );
