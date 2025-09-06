520bfdd88a8a4db4e0d9d9bdba372819932e7eb2
Temmacrown <temmacrown41@gmail.com>
2025-09-06 22:27:41 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,7 @@
-function UserProfile(props) {
-  return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
-      <h2 style={{ color: 'blue', marginBottom: '5px' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p style={{ fontStyle: 'italic' }}>Bio: {props.bio}</p>
-    </div>
-  );
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />;
 }
 
-export default UserProfile;
+export default ProfilePage;
