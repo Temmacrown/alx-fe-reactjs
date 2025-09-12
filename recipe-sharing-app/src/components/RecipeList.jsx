7a01ae9ec8b1c7e68df3fd81a3b7d6d612e6e8a7
Temmacrown <temmacrown41@gmail.com>
2025-09-12 14:18:10 +0100

    Create RecipeList.jsx

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,25 @@
+import { useRecipeStore } from '../store/recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  if (recipes.length === 0) {
+    return <p>No recipes yet. Add one!</p>;
+  }
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div
+          key={recipe.id}
+          style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px' }}
+        >
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
