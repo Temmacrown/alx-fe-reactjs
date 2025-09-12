fcc18edc5860e965bd5f0d71ea9f35633bab6aa9
Temmacrown <temmacrown41@gmail.com>
2025-09-12 14:25:46 +0100

    Create RecipeDetails.jsx

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,31 @@
+import { useParams, Link } from 'react-router-dom';
+import { useRecipeStore } from '../store/recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === Number(id))
+  );
+
+  if (!recipe) return <p>Recipe not found.</p>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+
+      <h3>Edit Recipe</h3>
+      <EditRecipeForm recipe={recipe} />
+
+      <DeleteRecipeButton id={recipe.id} />
+
+      <Link to="/" style={{ display: 'block', marginTop: '20px' }}>
+        ⬅ Back to Recipes
+      </Link>
+    </div>
+  );
+};
+
+export default RecipeDetails;
