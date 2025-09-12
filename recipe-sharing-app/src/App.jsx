d3ae72760c167565d86f7cbcc0638f3cd652d4e3
Temmacrown <temmacrown41@gmail.com>
2025-09-12 15:52:28 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,23 +1,39 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 import FavoritesList from "./components/FavoritesList";
 import RecommendationsList from "./components/RecommendationsList";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Recipes</Link> |{" "}
-        <Link to="/favorites">Favorites</Link> |{" "}
-        <Link to="/recommendations">Recommendations</Link>
-      </nav>
+      <div style={{ maxWidth: "600px", margin: "20px auto", fontFamily: "Arial" }}>
+        <h1>🍲 Recipe Sharing App</h1>
 
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/favorites" element={<FavoritesList />} />
-        <Route path="/recommendations" element={<RecommendationsList />} />
-      </Routes>
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+          <Link to="/favorites" style={{ marginRight: "10px" }}>Favorites</Link>
+          <Link to="/recommendations">Recommendations</Link>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
